734887aea00c1580410008078585c315d733c81b
iyz <iy.usa@outlook.com>
2025-04-26 17:23:02 -0400

    add X-API-KEY header to backend reqs, modularize code

--- cmd/health-monitor/main.go
+++ cmd/health-monitor/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/iyilmaz24/AWS-Cron-Lambda/internal"
@@ -14,25 +16,26 @@ func handler(ctx context.Context) (string, error) {
 	sites := os.Getenv("SITES_TO_MONITOR")
 	servers := os.Getenv("SERVERS_TO_MONITOR")
 
-	fmt.Println("Monitoring Sites:", sites)
-	fmt.Println("Monitoring Servers:", servers)
+	fmt.Println("***INFO: Monitoring Sites:", sites)
+	fmt.Println("***INFO: Monitoring Servers:", servers)
 
 	sitesList := strings.Split(sites, ",")
 	serversList := strings.Split(servers, ",")
 	unhealthyEndpoints := []string{}
+	client := &http.Client{Timeout: 7 * time.Second} // 7 second max timeout for requests
 
 	for _, site := range sitesList {
-		internal.CheckEndpointHealth(&unhealthyEndpoints, site)
+		internal.CheckEndpointHealth(client, &unhealthyEndpoints, site, "")
 	}
 
 	for _, server := range serversList {
-		internal.CheckEndpointHealth(&unhealthyEndpoints, server)
+		internal.CheckEndpointHealth(client, &unhealthyEndpoints, server, os.Getenv("NOTIFICATION_SERVER_API_KEY")) // backend servers require X-API-KEY header
 	}
 
 	allSuccessful := len(unhealthyEndpoints) == 0
-	err := internal.SendNotification(allSuccessful, unhealthyEndpoints)
+	err := internal.SendNotification(client, allSuccessful, unhealthyEndpoints)
 	if err != nil {
-		fmt.Println("Error sending notification:", err)
+		fmt.Println("***ERROR: Error sending notification:", err)
 		return "", err // this allows AWS to recognize the Lambda invocation as a failure
 	}
 
--- cmd/internal/helpers.go
+++ cmd/internal/helpers.go
@@ -27,17 +27,42 @@ type Notification struct {
 	AccessSecret           string   `json:"password"`
 }
 
-func CheckEndpointHealth(unhealthyEndpoints *[]string, endpoint string) {
-	resp, err := http.Get(endpoint)
-	if err != nil || resp.StatusCode >= 400 {
-		resp, err = http.Get(endpoint) // retry HTTP request
-		if err != nil || resp.StatusCode >= 400 {
-			*unhealthyEndpoints = append(*unhealthyEndpoints, endpoint) // if unsuccessful, add endpoint to slice
+func CheckEndpointHealth(client *http.Client, unhealthyEndpoints *[]string, endpoint string, apiKey string) {
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		fmt.Printf("***ERROR: Failed to create request for endpoint '%s': %v\n", endpoint, err)
+		*unhealthyEndpoints = append(*unhealthyEndpoints, endpoint)
+		return
+	}
+
+	if apiKey != "" {
+		req.Header.Set("X-API-Key", apiKey)
+	}
+
+	isUnhealthy := true
+
+	for attempt := 1; attempt <= 2; attempt++ { // max of 2 attempts
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("***ERROR: Attempt %d failed: network error: %v\n", attempt, err)
+			continue
 		}
+		resp.Body.Close()
+
+		if resp.StatusCode < 400 {
+			isUnhealthy = false
+			break
+		}
+	}
+
+	if isUnhealthy {
+		*unhealthyEndpoints = append(*unhealthyEndpoints, endpoint)
 	}
 }
 
-func SendNotification(allSuccessful bool, unhealthyEndpoints []string) error {
+func GetNotificationObject(allSuccessful bool, unhealthyEndpoints []string) Notification {
+
 	now := time.Now()
 	notificationDate := now.Format("2006-01-02") // "2006-01-02 15:04:05.999999999 -0700 MST" - Golang's reference timestamp
 	notificationTime := now.Format("15:04:05")
@@ -66,7 +91,7 @@ func SendNotification(allSuccessful bool, unhealthyEndpoints []string) error {
 		NotificationSMS:        true,
 		NotificationUrgency:    urgency,
 		NotificationRecipients: notificationRecipients,
-		NotificationStatus:     "Pending",
+		NotificationStatus:     "",
 		NotificationID:         notificationId,
 		NotificationType:       "CloudWatch CRON",
 		NotificationSource:     "AWS Lambda - Health Monitor",
@@ -78,7 +103,20 @@ func SendNotification(allSuccessful bool, unhealthyEndpoints []string) error {
 		AccessSecret:           "",
 	}
 
+	return notification
+}
+
+func SendNotification(client *http.Client, allSuccessful bool, unhealthyEndpoints []string) error {
+
 	notificationServerEndpoint := os.Getenv("NOTIFICATION_SERVER_ENDPOINT")
+	backendApiKey := os.Getenv("NOTIFICATION_SERVER_API_KEY")
+	if notificationServerEndpoint == "" || backendApiKey == "" {
+		return fmt.Errorf("Notification server endpoint and/or API key missing")
+	}
+
+	notification := GetNotificationObject(allSuccessful, unhealthyEndpoints)
+	notification.NotificationStatus = "1st Attempt"
+
 	jsonData, err := json.Marshal(notification)
 	if err != nil {
 		return fmt.Errorf("failed to marshal notification: %w", err)
@@ -89,32 +127,43 @@ func SendNotification(allSuccessful bool, unhealthyEndpoints []string) error {
 		if err != nil {
 			return nil, fmt.Errorf("failed to create request: %w", err)
 		}
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("X-API-Key", os.Getenv("NOTIFICATION_SERVER_API_KEY"))
 
-		client := &http.Client{Timeout: 5 * time.Second}
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("X-API-Key", backendApiKey)
 		return client.Do(req)
 	}
 
 	resp, err := sendNotification()
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println("First attempt to send notification failed:", err, "Status Code:", resp.StatusCode)
-		notification.NotificationStatus = "Retrying"
-		jsonData, _ = json.Marshal(notification)
-
-		time.Sleep(2 * time.Second) // wait before retrying
-
-		resp, err = sendNotification()
-		if err != nil {
-			fmt.Println("Second attempt to send notification failed with error:", err)
-			return fmt.Errorf("failed to send notification after retries: %w", err)
-		} else if resp.StatusCode >= 400 {
-			fmt.Println("Second attempt to send notification failed with status code:", resp.StatusCode)
-			return fmt.Errorf("failed to send notification after retries: status code %d", resp.StatusCode)
+	if err != nil {
+		fmt.Println("***ERROR: First attempt to send notification failed with error:", err)
+	} else {
+		defer resp.Body.Close()
+		if resp.StatusCode < 400 {
+			fmt.Println("***INFO: Notification sent successfully")
+			return nil
 		}
+		fmt.Printf("***ERROR: First attempt failed: received status code %d\n", resp.StatusCode)
 	}
 
-	defer resp.Body.Close()
-	fmt.Println("Notification sent successfully")
-	return nil
+	notification.NotificationStatus = "2nd Attempt"
+	jsonData, err = json.Marshal(notification)
+	if err != nil {
+		return fmt.Errorf("failed to marshal notification: %w", err)
+	}
+	time.Sleep(2 * time.Second) // wait before retrying
+
+	resp, err = sendNotification()
+	if err != nil {
+		fmt.Println("***ERROR: Second attempt to send notification failed with error:", err)
+		return fmt.Errorf("failed to send notification after retries: %w", err)
+	} else {
+		defer resp.Body.Close()
+		if resp.StatusCode < 400 {
+			fmt.Println("***INFO: Notification sent successfully")
+			return nil
+		}
+		fmt.Printf("***ERROR: Second attempt failed: received status code %d\n", resp.StatusCode)
+		return fmt.Errorf("failed to send notification after retries: %w", err)
+	}
+
 }
